18df7c54908623b2d8cae5e2f8d2b446d31959bf
vahid4 <vahid.gasanin1@gmail.com>
2022-11-06 21:28:48 +0100

    m

--- index.js
+++ index.js
@@ -47,9 +47,24 @@ console.log(isImranhere(niz));
 // const sredjeniniz=niz.flat();
 // console.log(sredjeniniz.sort().reverse())
 
-// Na osnovu stringa koji unosi korisnik napraviti niz, gde cemo ispitati da li se jagoda nalazi unutar tog niza. Ako se jagoda 
+// Na osnovu stringa koji unosi korisnik napraviti niz, gde cemo ispitati da li se jagoda nalazi unutar tog niza. Ako se jagoda
 //ne nalazi unutar niza, funkcija neka vrati taj niz poredjan abecedno, s tim sto ce se na prvom mestu dodati broj 10, a na poslednjem broj 100.
-// Dok ako se jagoda nalazi unutar niza vratiti poziciju gde se ona nalzi. 
-a=prompt("unesite string")
-console.log(a)
-console.log("nesto")
+// Dok ako se jagoda nalazi unutar niza vratiti poziciju gde se ona nalzi.
+
+
+// var nekafunk = () => {
+//   str = prompt("Unesite neki tekst");
+//   niz = str.split(" ");
+// //console.log(niz)
+//   if (niz.includes("jagoda")) {
+//     console.log(niz.indexOf("jagoda"));
+//   } else {
+//     console.log(niz.sort().unshift("10").push("100")) 
+     
+// }
+// };
+// nekafunk();
+
+niz=["vahid", "bostan", "knjiga"]
+console.log(niz.push("100"))
+
