ee543158dc6c26c5a50d66d68afdf7eb27463fc0
vahid4 <vahid.gasanin1@gmail.com>
2022-11-09 20:26:15 +0100

    commit

--- 3.cas/index.js
+++ 3.cas/index.js
@@ -7,3 +7,28 @@
 // ispisivanje(10);
 
 //callback function
+
+// function main(func) {
+//   console.log("ovo je kod koji se izvrsava unutar glavne funkcije.");
+//  func();
+// }
+
+// function callBack() {
+//   console.log("pozdrav ljudii ashdajbsdja sdh habsdk kabsdk adjbad a");
+// }
+
+// main(callBack);
+
+//------------------------------------
+
+function main(func) {
+    console.log("ovo je glavna funkcija.");
+   func(59,15,33);
+  }
+  
+  function callBack(arg1,arg2,arg3) {
+    let average=+((arg1+arg2+arg3)/3).toFixed(2);
+    console.log( `Aritmeticka sredina je ${average}`)
+  }
+    main(callBack);
+//    
