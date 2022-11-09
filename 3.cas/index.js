ae6d8bf19ae2c9fd2e58ee3d0f3394284924734e
vahid4 <vahid.gasanin1@gmail.com>
2022-11-09 21:04:15 +0100

    commit

--- 3.cas/index.js
+++ 3.cas/index.js
@@ -21,14 +21,48 @@
 
 //------------------------------------
 
-function main(func) {
-    console.log("ovo je glavna funkcija.");
-   func(59,15,33);
-  }
-  
-  function callBack(arg1,arg2,arg3) {
-    let average=+((arg1+arg2+arg3)/3).toFixed(2);
-    console.log( `Aritmeticka sredina je ${average}`)
-  }
-    main(callBack);
-//    
+// function main(func) {
+//     console.log("ovo je glavna funkcija.");
+//    func(59,15,33);
+//   }
+
+//   function callBack(arg1,arg2,arg3) {
+//     let average=+((arg1+arg2+arg3)/3).toFixed(2);
+//     console.log( `Aritmeticka sredina je ${average}`)
+//   }
+//     main(callBack);
+// -------------------------------
+//   OBJEKTI
+
+// const car = [{
+//   marka: "bmw",
+//   godiste: 2003,
+//   boja: "braon",
+// }];
+
+// console.log(car);
+
+// const osoba={
+//     JBMG:2506003710120,
+//     ime: "Mutemir",
+//     prezime:"Mukipersonalni",
+//     mesto:"Hadzet",
+//     imeIprezime:function(){
+//         return this.ime + " "+ this.prezime
+//     }
+// }
+// console.log(osoba.imeIprezime());
+
+//napraviti objekat student koji ce da ima vrednosti po zelji, svojstava ime,prezime,broj indeksa,ocene(niz svih ocena koje student ima u trenutnoj godini), prosek ocena funkcija koja na osnovu niza ocene vraca prosek datih ocena
+
+const student={
+    
+    ime: "Vahid",
+    prezime:"Gasanin",
+    broj_indeksa:"035030",
+    ocene:[6,9,8,6,7,6,9,6],
+    prosek:function () {
+        
+    }
+}
+console.log();
