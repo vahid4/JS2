d8a43c9b20480f2b55724316b6e587eecc29a362
vahid4 <vahid.gasanin1@gmail.com>
2022-11-11 21:16:00 +0100

    commit

--- 4.cas/index.js
+++ 4.cas/index.js
@@ -1,1 +1,26 @@
-console.log("nesto")
+const person = {
+  JMBG: 1404003783933,
+  ime: "Ahmed",
+  prezime: "Tutic",
+};
+
+// const x=person
+// x.ime="Muhamed"
+// console.log(x)
+// console.log(person)
+//-------------------------
+// delete person.JMBG;
+// console.log(person);
+// delete brise cio par i property i value
+
+// const a=JSON.stringify(person)
+// // console.log(a)
+// //stringify pretvara objekat u JSON string
+// const b=JSON.parse(a)
+// console.log(b)
+// //parse pretvara iz stringa u objekat
+
+niz=["ahmed", "vahid"]
+console.log(niz.join(" "))
+const a=JSON.stringify(niz);
+console.log(a)
