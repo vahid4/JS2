97019a832e9fdfe36b83b8da9df4b544e2fcd595
vahid4 <vahid.gasanin1@gmail.com>
2022-11-14 20:49:09 +0100

    commit

---
+++ 5.cas/index.js
@@ -0,0 +1,15 @@
+const vaha ={
+    firstName: "Vahid",
+    lastName: "Gashi",
+    language:"bosanski",
+    setLanguage:function (jezik) {
+        this.language=jezik
+        return this.language
+    },
+    setNickName:function () {
+    vaha.nickname=this.firstName.slice(0,2)+this.lastName.slice(0,2)
+       return vaha.nickname;
+    }
+}
+console.log(vaha.language);
+console.log(vaha.setLanguage("nemacki"));
