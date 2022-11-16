30fd770f7d4383be55339bc28257da587364fdf3
vahid4 <vahid.gasanin1@gmail.com>
2022-11-16 13:48:36 +0100

    dates

--- 5.cas/index.js
+++ 5.cas/index.js
@@ -14,3 +14,37 @@
 // console.log(vaha.setNickName())
 // console.log(vaha.language);
 // console.log(vaha.setLanguage("nemacki"));
+
+//---------domaci-------------------------
+// 2. Zadatak
+
+// const automobil = {
+//     marka: "Audi",
+//     model: "Q7",
+//     boja: "Bela",
+//     pogon: "quattro",
+//     menjac: "Automatik",
+//     km:240000,
+//     vlasnik:[062321552,063930630],
+//     garaza:{
+//         parking:"JKP Servis",
+//         vikend:"od 17 free",
+//         satnaKarta: "50",
+//         dnevnaKarta: "200",
+//         mesecnaKarta: "2000",
+//         platiZa: function(od, do) {
+      
+// }
+//     }
+// }
+// automobil.platiZa()
+//------------------------
+const d=new Date("2022-03-26")
+const v=new Date("2022-04-26")
+console.log(parseInt((d-v)/(1000*60*60*24),10))
+
+
+// var date1 = new Date("7/11/2010");
+// var date2 = new Date("7/15/2010");
+// var diffDays = parseInt((date2 - date1) / (1000 * 60 * 60 * 24), 10); 
+// console.log(diffDays)
