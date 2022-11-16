f733265b5cd84899c04309f519893c970fcc671b
vahid4 <vahid.gasanin1@gmail.com>
2022-11-16 13:26:36 +0100

    domaci

--- 5.cas/index.js
+++ 5.cas/index.js
@@ -1,16 +1,16 @@
-const vaha ={
-    firstName: "Vahid",
-    lastName: "Gashi",
-    language:"bosanski",
-    setLanguage:function (jezik) {
-        this.language=jezik
-        return this.language
-    },
-    setNickName:function () {
-    vaha.nickname=this.firstName.slice(0,2)+(this.lastName.slice(0,2)).toLocaleLowerCase()
-       return vaha.nickname;
-    }
-}
-console.log(vaha.setNickName())
-console.log(vaha.language);
-console.log(vaha.setLanguage("nemacki"));
+// const vaha ={
+//     firstName: "Vahid",
+//     lastName: "Gashi",
+//     language:"bosanski",
+//     setLanguage:function (jezik) {
+//         this.language=jezik
+//         return this.language
+//     },
+//     setNickName:function () {
+//     vaha.nickname=this.firstName.slice(0,2)+(this.lastName.slice(0,2)).toLocaleLowerCase()
+//        return vaha.nickname;
+//     }
+// }
+// console.log(vaha.setNickName())
+// console.log(vaha.language);
+// console.log(vaha.setLanguage("nemacki"));
