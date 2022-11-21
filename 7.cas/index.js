85ee5cd73e84d5f8415b0ab6a0b8d93fc76b8252
vahid4 <vahid.gasanin1@gmail.com>
2022-11-21 20:46:01 +0100

    commit

--- 7.cas/index.js
+++ 7.cas/index.js
@@ -1,19 +1,19 @@
-const automobil = {
-  marka: "Audi",
-  model: "Q7",
-  boja: "Bela",
-  pogon: "quattro",
-  menjac: "Automatik",
-  km: 240000,
-  vlasnik: ["062321552", "063930630"],
-  garaza: {
-    parking: "JKP Servis",
-    vikend: "od 17 free",
-    satnaKarta: 50,
-    dnevnaKarta: 200,
-    mesecnaKarta: 2000,
-  },
-};
+// const automobil = {
+//   marka: "Audi",
+//   model: "Q7",
+//   boja: "Bela",
+//   pogon: "quattro",
+//   menjac: "Automatik",
+//   km: 240000,
+//   vlasnik: ["062321552", "063930630"],
+//   garaza: {
+//     parking: "JKP Servis",
+//     vikend: "od 17 free",
+//     satnaKarta: 50,
+//     dnevnaKarta: 200,
+//     mesecnaKarta: 2000,
+//   },
+// };
 //napraviti funkciju koja ce promeniti vrednost satne dnevne i mesecne karte za po 20%
 //za bilo koji objekat koji ima properties: satnaKarta,dnevnaKarta,mesecnaKarta.
 
@@ -31,24 +31,35 @@ const automobil = {
 // }
 
 // console.log(povecaj.call(automobil.garaza))
-const abdurahman = "abdurahman";
-const selmanovic = "selmanovic";
-const godine = 14;
-const skola = "bratstvo";
+// const abdurahman = "abdurahman";
+// const selmanovic = "selmanovic";
+// const godine = 14;
+// const skola = "bratstvo";
 
-const person = {
-  abdurahman: abdurahman,
-  selmanovic: selmanovic,
-  godine: godine,
-  skola: skola,
-};
+// const person = {
+//   abdurahman: abdurahman,
+//   selmanovic: selmanovic,
+//   godine: godine,
+//   skola: skola,
+// };
 
-console.log(person);
+// console.log(person);
 
-const person2 = {
-  abdurahman,
-  selmanovic,
-  godine,
-  skola,
-};
-console.log(person2);
+// const person2 = {
+//   abdurahman,
+//   selmanovic,
+//   godine,
+//   skola,
+// };
+// console.log(person2);
+// //destructuring:
+
+// const automobil={
+//     marka:"Audi",
+//     model:"Q7",
+//     boja:"bela",
+// }
+// console.log(automobil.model)
+// const{ marka, model}=automobil; //destructuring
+// console.log(marka,model)
+
