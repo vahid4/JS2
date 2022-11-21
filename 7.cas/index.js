ff4032f45e1d94791e280efdccc398d7aba527f0
vahid4 <vahid.gasanin1@gmail.com>
2022-11-21 20:34:51 +0100

    commit

--- 7.cas/index.js
+++ 7.cas/index.js
@@ -1,29 +1,54 @@
 const automobil = {
-            marka: "Audi",
-             model: "Q7",
-             boja: "Bela",
-             pogon: "quattro",
-             menjac: "Automatik",
-             km:240000,
-             vlasnik:["062321552","063930630"],
-             garaza:{
-                 parking:"JKP Servis",
-                 vikend:"od 17 free",
-                 satnaKarta: 50,
-                 dnevnaKarta: 200,
-                 mesecnaKarta: 2000,
-                
-             }
-        }
+  marka: "Audi",
+  model: "Q7",
+  boja: "Bela",
+  pogon: "quattro",
+  menjac: "Automatik",
+  km: 240000,
+  vlasnik: ["062321552", "063930630"],
+  garaza: {
+    parking: "JKP Servis",
+    vikend: "od 17 free",
+    satnaKarta: 50,
+    dnevnaKarta: 200,
+    mesecnaKarta: 2000,
+  },
+};
 //napraviti funkciju koja ce promeniti vrednost satne dnevne i mesecne karte za po 20%
-//za bilo koji objekat koji ima properties: satnaKarta,dnevnaKarta,mesecnaKarta.        
-function povecaj() {
-    this.satnaKarta+=this.satnaKarta*0.2;
-    this.dnevnaKarta+=this.dnevnaKarta*0.2;
-    this.mesecnaKarta+=this.mesecnaKarta*0.2;
-    return `Nove cene su: satna karta ${this.satnaKarta} rsd, dnevna karta ${this.dnevnaKarta} rsd i mesecna karta ${this.mesecnaKarta} rsd.`
+//za bilo koji objekat koji ima properties: satnaKarta,dnevnaKarta,mesecnaKarta.
 
-}
+// function povecaj() {
+//     this.satnaKarta+=this.satnaKarta*0.2;
+//     this.dnevnaKarta+=this.dnevnaKarta*0.2;
+//     this.mesecnaKarta+=this.mesecnaKarta*0.2;
+//     //return `Nove cene su: satna karta ${this.satnaKarta} rsd, dnevna karta ${this.dnevnaKarta} rsd i mesecna karta ${this.mesecnaKarta} rsd.`
+//     return {
+//         satnaKarta:this.satnaKarta,
+//         dnevnaKarta:this.dnevnaKarta,
+//         mesecnaKarta:this.mesecnaKarta,
+//     }
 
-console.log(povecaj.call(automobil.garaza))
-console.log(povecaj.apply(automobil.garaza))
+// }
+
+// console.log(povecaj.call(automobil.garaza))
+const abdurahman = "abdurahman";
+const selmanovic = "selmanovic";
+const godine = 14;
+const skola = "bratstvo";
+
+const person = {
+  abdurahman: abdurahman,
+  selmanovic: selmanovic,
+  godine: godine,
+  skola: skola,
+};
+
+console.log(person);
+
+const person2 = {
+  abdurahman,
+  selmanovic,
+  godine,
+  skola,
+};
+console.log(person2);
