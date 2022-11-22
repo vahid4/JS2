ddd1924eba1fae3ab9b08413d1578e926bfc7323
vahid4 <vahid.gasanin1@gmail.com>
2022-11-22 14:31:09 +0100

    commit

--- 7.cas/index.js
+++ 7.cas/index.js
@@ -73,7 +73,7 @@
 
 // //Napraviti novi niz (punoletni) koji ce sadrzati one elemente cija je starost veca od 17.
 
-// const punoletni = 
+// const punoletni =
 //   godine.filter((el) => {
 //     return el.starost > 17;
 //   })
@@ -83,3 +83,64 @@
 //napraviti niz objekata gde ce svaki objekat sadrzati:
 //ime,prezime,starost,bojaOciju,bojaKose,polozenC.
 //napraviti funkciju koja pravi novi niz i na kraju ga vraca, koja ce sadrzati one elemente cija boja ociju je braon i koji nisu polozili C.
+
+const niz = [
+  {
+    ime: "Vahid",
+    prezime: "Gasanin",
+    starost: 19,
+    bojaOciju: "braon",
+    bojaKose: "braon",
+    polozenC: true,
+  },
+  {
+    ime: "Imran",
+    prezime: "Hazirovic",
+    starost: 20,
+    bojaOciju: "braon",
+    bojaKose: "plava",
+    polozenC: true,
+  },
+  {
+    ime: "Ahmed",
+    prezime: "Krkmisevic",
+    starost: 19,
+    bojaOciju: "plava",
+    bojaKose: "crna",
+    polozenC: false,
+  },
+  {
+    ime: "Muhamed",
+    prezime: "Krkmisevic",
+    starost: 14,
+    bojaOciju: "braon",
+    bojaKose: "crna",
+    polozenC: false,
+  },
+  {
+    ime: "Abdurahman",
+    prezime: "Selimovic",
+    starost: 16,
+    bojaOciju: "braon",
+    bojaKose: "crna",
+    polozenC: false,
+  },
+  {
+    ime: "Amina",
+    prezime: "Besirovic",
+    starost: 18,
+    bojaOciju: "plava",
+    bojaKose: undefined,
+    polozenC: true,
+  },
+  {
+    ime: "Amina",
+    prezime: "Marovac",
+    starost: 19,
+    bojaOciju: "braon",
+    bojaKose: "braon",
+    polozenC: true,
+  },
+  
+   
+];
