ff337f22120fdacb1b3228f9f9b8ed68d33cb725
vahid4 <vahid.gasanin1@gmail.com>
2022-11-23 20:26:09 +0100

    is()

--- 8.cas/index.js
+++ 8.cas/index.js
@@ -6,6 +6,16 @@ const person = {
   bojaKose: "crna",
   polozenC: false,
 };
+const person2=person;
+
+const osoba = {
+    ime: "Abdurahman",
+    prezime: "Selimovic",
+    starost: 16,
+    bojaOciju: "braon",
+    bojaKose: "crna",
+    polozenC: false,
+  };
 
 //freeze() metoda cini objekat immutable(onemogucava promene na njemu).
 // Object.freeze(person)
@@ -25,5 +35,14 @@ const person = {
 //   console.log(vrednosti)
 
   //keys() metoda vraca niz kljuceva datog objekta
-  const vrednosti=Object.keys(person)
-  console.log(vrednosti)
+//   const vrednosti=Object.keys(person)
+//   console.log(vrednosti)
+
+//entries() metoda vraca niz nizova(svaki podniz je name:value par)
+// const vrednosti=Object.entries(person)
+//   console.log(vrednosti)
+//   console.log(vrednosti.flat())
+
+// is() ispituje identicku jednakost
+console.log(Object.is(person, person2))
+console.log(Object.is(person, osoba))
