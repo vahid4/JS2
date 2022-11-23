af5f3a4d07da5282fbc2400926f8bc2f55122c2d
vahid4 <vahid.gasanin1@gmail.com>
2022-11-23 20:11:05 +0100

    freeze(), seal(), values(), keys().

--- 8.cas/index.js
+++ 8.cas/index.js
@@ -1,1 +1,29 @@
-console.log("nesto")
+const person = {
+  ime: "Abdurahman",
+  prezime: "Selimovic",
+  starost: 16,
+  bojaOciju: "braon",
+  bojaKose: "crna",
+  polozenC: false,
+};
+
+//freeze() metoda cini objekat immutable(onemogucava promene na njemu).
+// Object.freeze(person)
+// person.ime="Vahid"
+// console.log(person)
+
+//-----------------------------------------------------------
+
+//seal() metoda dozvoljava promene vrednosti svojstava, ali ne dozvoljava dodavanje novih ili brisanje.
+// Object.seal(person);
+// delete person.ime;
+// console.log(person);
+
+
+  //values() metoda vraca niz vrednosti datog objekta
+//   const vrednosti=Object.values(person)
+//   console.log(vrednosti)
+
+  //keys() metoda vraca niz kljuceva datog objekta
+  const vrednosti=Object.keys(person)
+  console.log(vrednosti)
