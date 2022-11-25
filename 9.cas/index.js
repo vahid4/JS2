417c45be37e8e5672edd25211cfa3aabafc3a6ee
vahid4 <vahid.gasanin1@gmail.com>
2022-11-25 20:55:47 +0100

    commit

--- 9.cas/index.js
+++ 9.cas/index.js
@@ -23,6 +23,24 @@
 // console.log(skloniDuplikat(niz));
 
 // ... spread operator nam sluzi za kopiranje svih elemenata niza ili objekta u drugi niz ili objekat
-niz = [5, "nesto", 3, 9, 3, "nesto"];
-niz2 = [...niz];
-console.log(niz2);
+// niz = [5, "nesto", 3, 9, 3, "nesto"];
+// niz2 = [...niz];
+// //console.log(niz2);
+// niz3 = [...niz, ...niz2];
+// console.log(niz3);
+// niz4 = [15, ...niz2, 2];
+// console.log(niz4);
+
+// const obj = {
+//   ime: "Muha",
+//   prezime: "HAzirovic",
+//   starost: 17,
+// };
+
+// const noviObj = {
+//   ...obj,
+//   bojaKOse: "crna",
+//   kovrdzav: false,
+// };
+
+// console.log(noviObj);
