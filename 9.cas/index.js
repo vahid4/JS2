c4d293bb4e10686cef625c757a12d0b6cda29f59
vahid4 <vahid.gasanin1@gmail.com>
2022-11-25 20:37:53 +0100

    commit

--- 9.cas/index.js
+++ 9.cas/index.js
@@ -1,1 +1,13 @@
-console.log("nestoooo")
+//napraviti funkciju koja nam vraca novi niz koji nema duplikata
+niz = [5, "nesto", 3, 9, 3, "nesto"];
+
+// const skloniDuplikat = (arr) => {
+//   novi = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!novi.includes(arr[i])) {
+//       novi.push(arr[i]);
+//     }
+//   }
+//   return novi;
+// };
+// console.log(skloniDuplikat(niz));
