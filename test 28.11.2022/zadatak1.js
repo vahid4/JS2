a1aa383bf2507148642e735a52c836767cfa96ac
vahid4 <vahid.gasanin1@gmail.com>
2022-11-28 20:46:52 +0100

    test

--- test 28.11.2022/zadatak1.js
+++ test 28.11.2022/zadatak1.js
@@ -1,1 +1,10 @@
-console.log(5);
+/*Uneti string se šifruje tako što se zamenjuje redosled znacima u stringu.
+Prvi i drugi zamene mesta, zatim 3. i 4. zamene mesta itd. Npr. za string 'Pera ima devojku',
+treba dobiti 'ePari amd vejouk'. Ako string ima neparan broj znakova, poslednji znak se ne dira.*/
+
+let niz = prompt("Unesite neki string");
+let novi = "";
+
+let a = niz.split("");
+
+console.log(a);
