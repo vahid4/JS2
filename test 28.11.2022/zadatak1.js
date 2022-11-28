b36384af5f733a17fb4f5b6fb82808c9cfcdd689
vahid4 <vahid.gasanin1@gmail.com>
2022-11-28 20:57:18 +0100

    commit

--- test 28.11.2022/zadatak1.js
+++ test 28.11.2022/zadatak1.js
@@ -3,8 +3,14 @@ Prvi i drugi zamene mesta, zatim 3. i 4. zamene mesta itd. Npr. za string 'Pera 
 treba dobiti 'ePari amd vejouk'. Ako string ima neparan broj znakova, poslednji znak se ne dira.*/
 
 let niz = prompt("Unesite neki string");
-let novi = "";
-
+let parni = "";
+let neparni = "";
 let a = niz.split("");
-
+for (let i = 0; i <= a.length - 1; i++) {
+  if (i % 2 === 0) {
+    parni += a[i];
+  } else neparni += a[i];
+}
 console.log(a);
+console.log(parni);
+console.log(neparni);
