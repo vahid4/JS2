c61d8abb49be383993acacbc119949695e384d20
vahid4 <vahid.gasanin1@gmail.com>
2022-12-05 20:32:03 +0100

    commit

--- 10.cas/index.js
+++ 10.cas/index.js
@@ -1,11 +1,13 @@
-const str = "Pera ima devojku";
+const str = "Pera ima devojku.";
 
 function nekaF(parameter) {
   let novistr = "";
   for (let i = 0; i < parameter.length; i++) {
-    if (i % 2 === 0) {
+    if (parameter.length % 2 !== 0 && i === parameter.length - 1) {
+      novistr += parameter[i];
+    } else if (i % 2 === 0) {
       novistr += parameter[i + 1];
-    }
+    } else if (i % 2 !== 0) novistr += parameter[i - 1];
   }
   return novistr;
 }
