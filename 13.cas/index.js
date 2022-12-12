39e39edef47c2213be0e539c1792baf12ed0e772
vahid4 <vahid.gasanin1@gmail.com>
2022-12-12 20:05:51 +0100

    userFunction

--- 13.cas/index.js
+++ 13.cas/index.js
@@ -1,23 +1,45 @@
-const person = {
-  firstName: "Muhamed",
-  lastName: "Krkmisevic",
+// const person = {
+//   firstName: "Muhamed",
+//   lastName: "Krkmisevic",
+// };
+
+// person.fullName = function () {
+//   return this.firstName + " " + this.lastName;
+// };
+
+// console.log(person);
+
+// function User(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
+// const talib = new User("Talib", "Dolovac");
+// console.log(talib);
+
+// User.prototype.fullName = function () {
+//   return this.firstName + " " + this.lastName;
+// };
+
+// console.log(talib.fullName());
+
+// 2. opcija je preko funkcije na sledeci nacin:
+
+//unutar objekta userFunctions stavljamo sve metode koje zelino dodati
+//u objekte napravljene na sledeci nacin
+
+const userFunctions = {
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
 };
 
-person.fullName = function () {
-  return this.firstName + " " + this.lastName;
-};
-
-console.log(person);
-
-function User(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+function createUser(firstName, lastName) {
+  const newUser = Object.create(userFunctions);
+  newUser.firstName = firstName;
+  newUser.lastName = lastName;
+  return newUser;
 }
-const talib = new User("Talib", "Dolovac");
-console.log(talib);
 
-User.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName;
-};
-
-console.log(talib.fullName());
+const vahid = createUser("Vahid", "Gasanin");
+console.log(vahid);
+console.log(vahid.fullName());
