3f91aea1b3650f2093aacf91bdb238262ead7900
vahid4 <vahid.gasanin1@gmail.com>
2022-12-12 19:57:08 +0100

    constructor function

--- 13.cas/index.js
+++ 13.cas/index.js
@@ -1,1 +1,23 @@
-console.log("nesto");
+const person = {
+  firstName: "Muhamed",
+  lastName: "Krkmisevic",
+};
+
+person.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+console.log(person);
+
+function User(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+const talib = new User("Talib", "Dolovac");
+console.log(talib);
+
+User.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+console.log(talib.fullName());
