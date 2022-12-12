9c27f5e2d75a7e44875ae04f652ddc356e10f543
vahid4 <vahid.gasanin1@gmail.com>
2022-12-12 20:42:15 +0100

    userFunction

---
+++ 13.cas/app2.js
@@ -0,0 +1,8 @@
+//JavaScript moduli(modules)
+//Moduli nam dozvoljavaju da podelimo nas kod u vise fajlova
+// za pravljenje modula koristimo
+// import i
+// export
+
+import zbir from "./function.js";
+console.log(zbir(4, 9));
---
+++ 13.cas/function.js
@@ -0,0 +1,9 @@
+const zbir = function (broj1, broj2) {
+  return broj1 + broj2;
+};
+// console.log(zbir(4, 3));
+
+//dva nacina exporta:
+// export zbir
+// export default zbir
+export default zbir;
