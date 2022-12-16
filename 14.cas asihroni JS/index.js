778c532d595ab5e9f41f9a0f937ecdcc14e7c66d
vahid4 <vahid.gasanin1@gmail.com>
2022-12-16 19:57:45 +0100

    commit

--- 14.cas asihroni JS/index.js
+++ 14.cas asihroni JS/index.js
@@ -1,6 +1,17 @@
+// function myFunc() {
+//   let broj = 4;
+//   let predmet = "stolice";
+//   return `U prostoriji imamo ${broj} ${predmet}`;
+// }
+// console.log(myFunc());
+
 function myFunc() {
   let broj = 4;
   let predmet = "stolice";
-  return `U prostoriji imamo ${broj} ${predmet}`;
+  function drugaFunkcija(arg) {
+    console.log(arg + 5);
+  }
+  drugaFunkcija(6);
+  return "To je bio primer closure u JavaScriptu";
 }
 console.log(myFunc());
