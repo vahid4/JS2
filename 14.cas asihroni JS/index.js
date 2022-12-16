10967df1e94553de9a9c223cfe2f73626becd93a
vahid4 <vahid.gasanin1@gmail.com>
2022-12-16 20:33:30 +0100

    set

--- 14.cas asihroni JS/index.js
+++ 14.cas asihroni JS/index.js
@@ -25,3 +25,31 @@
 //   return sporednaF();
 // }
 // console.log(nekaF());
+
+//JavaScripts sets
+// setovi predstavljaju kolekciju unikatnih vrednosti
+// svaka vrednost se pojavljuje samo jednom
+
+// Kreiranje seta:
+
+const letters = new Set(["a", "b", "c", "d", "c"]);
+console.log(letters);
+
+// Metode:
+
+// add()-dodaje novi element u setu
+letters.add("m");
+console.log(letters);
+
+// delete()- brise specifican element u setu
+letters.delete("m");
+console.log(letters);
+
+// has()- vraca boolean vrednost-da li se element nalazi u setu
+console.log(letters.has("m"));
+
+// forEach()-poziva callback funkciju za svaki element
+letters.forEach((el) => console.log(el));
+
+//values() vraca iterator sa vrednostima datog seta
+console.log(letters.values());
