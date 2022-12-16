48ca829193391b50d70d4fc11f48da3ba0647893
vahid4 <vahid.gasanin1@gmail.com>
2022-12-16 20:12:38 +0100

    commit

--- 14.cas asihroni JS/index.js
+++ 14.cas asihroni JS/index.js
@@ -5,13 +5,23 @@
 // }
 // console.log(myFunc());
 
-function myFunc() {
-  let broj = 4;
-  let predmet = "stolice";
-  function drugaFunkcija(arg) {
-    console.log(arg + broj);
+// function myFunc() {
+//   let broj = 4;
+//   let predmet = "stolice";
+//   function drugaFunkcija(arg) {
+//     console.log(arg + broj);
+//   }
+//   drugaFunkcija(10);
+//   return "To je bio primer closure u JavaScriptu";
+// }
+// console.log(myFunc());
+
+function nekaF() {
+  console.log("glavna funckija");
+  function sporednaF() {
+    console.log("sporedna f");
+    return 5;
   }
-  drugaFunkcija(10);
-  return "To je bio primer closure u JavaScriptu";
+  return sporednaF();
 }
-console.log(myFunc());
+console.log(nekaF());
