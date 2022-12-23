7cea5832d65135eae8a11819e5fd611a56bb4487
vahid4 <vahid.gasanin1@gmail.com>
2022-12-23 20:23:59 +0100

    web api

--- 16.cas/index.js
+++ 16.cas/index.js
@@ -1,5 +1,19 @@
-console.log(1);
-console.log(2);
-setTimeout(() => console.log(3), 1000, 1);
-console.log(4);
-setTimeout(() => console.log(5), 1000);
+// console.log(1);
+// console.log(2);
+// setTimeout(() => console.log(3), 1000, 1);
+// console.log(4);
+// setTimeout(() => console.log(5), 1000);
+
+// Promise je JavaScript objekat koji predstavlja mesto za rezultate
+// asihrone funkcije sve dok traje izvrsavanje asihrone operacije
+
+const imatiCas = new Promise((resolve, reject) => {
+  const response = 2000;
+  if (response === 200) {
+    resolve();
+  } else reject();
+});
+
+imatiCas
+  .then(() => console.log("desio se RESOLVE.Imali smo cas"))
+  .catch(() => console.log("desio se REJECT.Nismo imali smo cas"));
