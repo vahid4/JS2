bdca64ee867df370505dd095e3a209c31df44064
vahid4 <vahid.gasanin1@gmail.com>
2022-12-25 15:31:20 +0100

    promise

--- 12.cas/index.js
+++ 12.cas/index.js
@@ -1,36 +1,37 @@
-// var library = [
-//   { author: "Bill Gates", title: "The Road Ahead", libraryID: 1254 },
-//   { author: "Steve Jobs", title: "Walter Isaacson", libraryID: 4264 },
-//   {
-//     author: "Suzanne Collins",
-//     title: "Mockingjay: The Final Book of The Hunger Games",
-//     libraryID: 3245,
-//   },
-// ];
+var library = [
+  { author: "Bill Gates", title: "The Road Ahead", libraryID: 1254 },
+  { author: "Steve Jobs", title: "Walter Isaacson", libraryID: 4264 },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    libraryID: 3245,
+  },
+];
 
-// function sortirajPoTitlu(par) {
-//   const novi = par.map((el) => {
-//     return el.title;
-//   });
-//   novi.sort();
-//   const novi2 = [];
-//   for (let i = 0; i < par.length; i++) {
-//     if (
-//       par.find((element) => {
-//         return element.title === novi[i];
-//       })
-//     ) {
-//       novi2.push(
-//         par.find((element) => {
-//           return element.title === novi[i];
-//         })
-//       );
-//     }
-//   }
-//   return novi2;
-// }
+function sortirajPoTitlu(par) {
+  const novi = par.map((el) => {
+    return el.title;
+  });
+  novi.sort();
+  console.log(novi);
+  const novi2 = [];
+  for (let i = 0; i < par.length; i++) {
+    if (
+      par.find((element) => {
+        return element.title === novi[i];
+      })
+    ) {
+      novi2.push(
+        par.find((element) => {
+          return element.title === novi[i];
+        })
+      );
+    }
+  }
+  return novi2;
+}
 
-//console.log(sortirajPoTitlu(library));
+console.log(sortirajPoTitlu(library));
 //---------------------------------------------
 // niz = ["a", "v", "c", "P"];
 // niz2 = [1, 2, 3, 96, -63];
@@ -79,18 +80,18 @@
 // vahid.incrementscore();
 // console.log(vahid);
 
-function napraviAuto(marka, boja, godinaProizvodnje, udaran) {
-  this.marka = marka;
-  this.boja = boja;
-  this.godinaProizvodnje = godinaProizvodnje;
-  this.udaran = udaran;
-}
+// function napraviAuto(marka, boja, godinaProizvodnje, udaran) {
+//   this.marka = marka;
+//   this.boja = boja;
+//   this.godinaProizvodnje = godinaProizvodnje;
+//   this.udaran = udaran;
+// }
 
-napraviAuto.prototype.sigurnoLupan = function () {
-  this.udaran = true;
-};
+// napraviAuto.prototype.sigurnoLupan = function () {
+//   this.udaran = true;
+// };
 
-const peglica = new napraviAuto("fiat", "crvena", 1986, false);
-console.log(peglica);
-peglica.sigurnoLupan();
-console.log(peglica);
+// const peglica = new napraviAuto("fiat", "crvena", 1986, false);
+// console.log(peglica);
+// peglica.sigurnoLupan();
+// console.log(peglica);
