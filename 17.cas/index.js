4619031b5874a53320058593ed42427576097952
vahid4 <vahid.gasanin1@gmail.com>
2022-12-25 17:10:30 +0100

    promise interview question

--- 17.cas/index.js
+++ 17.cas/index.js
@@ -1,7 +1,22 @@
-for (let i = 0; i < 10; i++) {
-  setTimeout(() => console.log(i), 0);
-}
+// // let i const promenljive obezbedjuju block scope.
+// for (let i = 0; i < 10; i++) {
+//   setTimeout(() => console.log(i), 0);
+// }
 
-for (var i = 0; i < 10; i++) {
-  setTimeout(() => console.log(i), 0);
-}
+// // promenljive definisane preko VAR keyworda su global scope promenljive
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(() => console.log(i), 0);
+// }
+
+const promise = new Promise((resolve, reject) => {
+  resolve();
+});
+
+promise.then(() => {
+  promise.then(() => console.log(2));
+  console.log(1);
+});
+
+promise.then(() => {
+  console.log(3);
+});
