43072e7a706236e7bdd1a2ff0ec620e3f05b8906
vahid4 <vahid.gasanin1@gmail.com>
2022-12-30 20:05:50 +0100

    class

--- 18.cas(klase)/index.js
+++ 18.cas(klase)/index.js
@@ -1,16 +1,29 @@
 // Node je runtime koji vrsi egzekuciju i kompajlovanje javascript koda
 // Node ima mnostvo svojih modula, najpoznatiji su fs(fileSystem) i http modul
 
-class Car {
-  constructor(name, type) {
+// class Car {
+//   constructor(name, type) {
+//     this.name = name;
+//     this.type = type;
+//   }
+//   fullName() {
+//     return console.log(this.name + " " + this.type);
+//   }
+// }
+
+// const newCar = new Car("audi", "Limo");
+// console.log(newCar);
+// newCar.fullName();
+
+class Person {
+  constructor(name, surname) {
     this.name = name;
-    this.type = type;
+    this.surname = surname;
   }
   fullName() {
-    return console.log(this.name + " " + this.type);
+    return console.log(this.name + " " + this.surname);
   }
 }
 
-const newCar = new Car("audi", "Limo");
-console.log(newCar);
-newCar.fullName();
+const Vahid = new Person("Vahid", "Gasanin");
+Vahid.fullName();
