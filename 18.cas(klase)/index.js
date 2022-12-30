1fa21076057c34c6bcf0f42e9508cde3ecd4edd4
vahid4 <vahid.gasanin1@gmail.com>
2022-12-30 20:01:36 +0100

    class

--- 18.cas(klase)/index.js
+++ 18.cas(klase)/index.js
@@ -1,2 +1,16 @@
 // Node je runtime koji vrsi egzekuciju i kompajlovanje javascript koda
 // Node ima mnostvo svojih modula, najpoznatiji su fs(fileSystem) i http modul
+
+class Car {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  fullName() {
+    return console.log(this.name + " " + this.type);
+  }
+}
+
+const newCar = new Car("audi", "Limo");
+console.log(newCar);
+newCar.fullName();
