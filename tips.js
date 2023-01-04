ff35d67daccd4d95e01754f204efbe9577b02a4f
vahid4 <vahid.gasanin1@gmail.com>
2023-01-04 19:58:55 +0100

    class

--- tips.js
+++ tips.js
@@ -1,1 +1,3 @@
 //klase su izrada modela podataka unutar aplikacije(web ili neke druge)
+// svaka klasa mora imati metodu constructor(){}, a ona vrsi funkciju glavne funkcije(metode) unutar klase
+// pomocu concturctor metode se inicijalizuju glavni podaci unutar klase
