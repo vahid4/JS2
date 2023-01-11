ef4646abd1d02cdb968c5244e0028e3489b9171d
vahid4 <vahid.gasanin1@gmail.com>
2023-01-11 20:57:15 +0100

    DOM

---
+++ 21.cas/index.js
@@ -0,0 +1,17 @@
+var pozadina = document.getElementById("random");
+var promeniPozadinu = document.getElementById("bojaPozadine");
+promeniPozadinu.addEventListener("click", () => {
+  pozadina.style.backgroundColor = "red";
+});
+
+var naslov = document.getElementById("naslov");
+var promeniNaslov = document.getElementById("promeniNaslov");
+promeniNaslov.addEventListener("click", () => {
+  naslov.style.color = "blue";
+});
+
+var paragraf = document.getElementById("nekop");
+var promeniP = document.getElementById("promenip");
+promeniP.addEventListener("click", () => {
+  paragraf.innerText = "nesto bilo staaaaa";
+});
