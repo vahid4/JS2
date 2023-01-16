3575035d52f31f041266abbad8b33bf9857a52a7
vahid4 <vahid.gasanin1@gmail.com>
2023-01-16 20:57:12 +0100

    props

--- nitshop/src/App.js
+++ nitshop/src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import StyledButton from "./components/styledButton/StyledButton";
+import StyledHeader from "./components/styledButton/StyledHeader";
 
 function App() {
   return (
     <div className="App">
+      <StyledHeader innerText="THISSSS ISSSSS HEADDDDDDEEEERRRRRR" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Vahid je</p>
+
+        <StyledButton innerText="nestooo" />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ nitshop/src/components/styledButton/StyledHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styledButton.css";
+
+const StyledHeader = (props) => {
+  return <header>{props.innerText}</header>;
+};
+
+export default StyledHeader;
