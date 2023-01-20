c4808c59685acd306aa57034e167ef8fb50e90e3
vahid4 <vahid.gasanin1@gmail.com>
2023-01-20 20:40:42 +0100

    hooks

--- nitshop/src/App.js
+++ nitshop/src/App.js
@@ -4,32 +4,16 @@ import "./App.css";
 import StyledButton from "./components/styledButton/StyledButton";
 import StyledHeader from "./components/styledButton/StyledHeader";
 import StyledInput from "./components/StyledInput/StyledInput";
+import StyledForm from "./components/StyledForm/StyledForm";
 
 function App() {
-  const [innerText, setInnerText] = useState("");
-  const [clicked, setClicked] = useState(false);
-  const [inputValue, setInputValue] = useState();
-
-  useEffect(() => {
-    clicked ? setInnerText("Clicked") : setInnerText("Click me");
-  }, [clicked]);
-
-  function buttonClick() {
-    setClicked(!clicked);
-  }
-
-  function onChangeFunction(e) {
-    setInputValue(e.target.value);
-    console.log(inputValue);
-  }
-
   return (
     <div className="App">
       <StyledHeader innerText="THISSSS ISSSSS HEADDDDDDEEEERRRRRR" />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
-        <StyledButton innerText={innerText} onClickHandler={buttonClick} />
+
+        <StyledForm />
         <a
           className="App-link"
           href="https://reactjs.org"
--- nitshop/src/components/StyledForm/StyledForm.jsx
+++ nitshop/src/components/StyledForm/StyledForm.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import StyledInput from "../StyledInput/StyledInput";
 import StyledButton from "../styledButton/StyledButton";
+import { useState, useEffect } from "react";
+import "./StyledForm.css";
+
 function StyledForm() {
+  const [innerText, setInnerText] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [inputValue, setInputValue] = useState();
+
+  useEffect(() => {
+    clicked ? setInnerText("Clicked") : setInnerText("Click me");
+  }, [clicked]);
+
+  function buttonClick() {
+    setClicked(!clicked);
+  }
+
+  function onChangeFunction(e) {
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  }
   return (
     <form>
       <label>Username</label>
-      <StyledInput />
-      <StyledButton />
+      <StyledInput onChangeHandler={onChangeFunction} value={inputValue} />
+      <StyledButton innerText={innerText} onClickHandler={buttonClick} />
     </form>
   );
 }
