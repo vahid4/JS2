bb1b0200029e754a775a30792166ade43642a7e3
vahid4 <vahid.gasanin1@gmail.com>
2023-02-08 20:42:12 +0100

    styled-components

---
+++ random/src/util/theme.js
@@ -0,0 +1,9 @@
+export const accentColor = "#e88f23";
+export const secondColor = "#F0F0E9";
+
+export const fontWeight = {
+  light: "100",
+  medium: "400",
+  mediumBold: "600",
+  bold: "800",
+};
