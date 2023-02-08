d74a37d6075fabc6aaf5c271d00fe96cce9ed5c1
vahid4 <vahid.gasanin1@gmail.com>
2023-02-08 19:47:17 +0100

    styled-components

--- random/src/components/PrimaryButton/PrimaryButton.jsx
+++ random/src/components/PrimaryButton/PrimaryButton.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 const PrimaryButton = styled.button`
   border: 2px solid
-    ${(props) => (props.borderColor ? props.borderColor : "pink")};
-  color: ${(props) => (props.primary ? "pink" : "white")};
+    ${(props) => (props.borderColor ? props.borderColor : "red")};
+  color: ${(props) => (props.primary ? "pink" : "darkblue")};
   background-color: ${(props) => (props.primary ? "white" : "pink")};
   padding: ${(props) => (props.padding ? props.padding : 20)}px;
   opacity: ${(props) => (props.opacity ? props.opacity : 1)};
