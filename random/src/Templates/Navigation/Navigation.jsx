5369f881f2569e08c319bc3cd849fa760f3d125a
vahid4 <vahid.gasanin1@gmail.com>
2023-02-10 20:16:07 +0100

    styled-components

--- random/src/Templates/Navigation/Navigation.jsx
+++ random/src/Templates/Navigation/Navigation.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import CustomDiv from "../../components/CustomDiv/CustomDiv";
 import TopRow from "./TopRow";
 
 const Navigation = () => {
   return (
     <Grid container direction="row">
-      <CustomDiv padding="0px 20px" border="0px" width="100%">
-        <TopRow />
-      </CustomDiv>
+      <TopRow />
     </Grid>
   );
 };
--- random/src/Templates/Navigation/TopRow.jsx
+++ random/src/Templates/Navigation/TopRow.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import Grid from "@mui/material";
+import Grid from "@mui/material/Grid";
 import CustomDiv from "../../components/CustomDiv/CustomDiv";
+import { colors } from "../../util/theme";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const TopRow = () => {
   return (
     <Grid container direction="row">
-      <CustomDiv bgColor={color}></CustomDiv>
+      <CustomDiv
+        bgColor={colors.secondColor}
+        width="100%"
+        height="30px"
+        padding="0px 5%"
+        border="0px"
+      >
+        <PhoneIcon />
+        <p>+381669754145</p>
+      </CustomDiv>
     </Grid>
   );
 };
