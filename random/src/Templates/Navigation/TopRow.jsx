3918d58b290aac0a4f2bad012a9af5d6aec38b91
vahid4 <vahid.gasanin1@gmail.com>
2023-02-15 19:35:48 +0100

    styled-components

--- random/src/Templates/Navigation/TopRow.jsx
+++ random/src/Templates/Navigation/TopRow.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import CustomDiv from "../../components/CustomDiv/CustomDiv";
-import { colors, fontSize, fontStyle } from "../../util/theme";
+import { colors, fontSize } from "../../util/theme";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import TwitterIcon from "@mui/icons-material/Twitter";
@@ -13,38 +13,66 @@ import { getScreenWidth } from "../../util/helpers";
 const TopRow = () => {
   const screenWidth = getScreenWidth();
 
+  console.log(screenWidth);
+
   return (
     <CustomDiv
       bgColor={colors.secondColor}
+      display="flex"
       width="100%"
       height="30px"
       padding="0px 5%"
       border="0px"
     >
       <Grid container direction="row">
-        <Grid item lg={6} spacing={0}>
+        <Grid item md={6} lg={6}>
           <CustomDiv display="flex" alignItems="center">
-            <PhoneIcon style={{ fontSize: fontSize.normal }} />
-
-            <Text fontSize={fontSize.small}>+381669754145</Text>
-            <EmailIcon
-              style={{ fontSize: fontSize.normal, margin: "0px 0px 0px 5px" }}
+            <PhoneIcon
+              style={{
+                fontSize: fontSize.normal,
+              }}
             />
-            <Text fontSize={fontSize.small}>vahid.gasanin2@gmail.com</Text>
-          </CustomDiv>
-          <Grid>
-            <CustomDiv display="flex" alignItems="center" textAlign="right">
-              <TwitterIcon
-                style={{ fontSize: fontSize.normal, margin: "0px 0px 0px 5px" }}
+            <Text fontSize={fontSize.small}>+38163333333</Text>
+            <CustomDiv
+              display="flex"
+              alignItems="center"
+              margin="0px 0px 0px 5px"
+            >
+              <EmailIcon
+                style={{
+                  fontSize: fontSize.normal,
+                }}
               />
-              <InstagramIcon
-                style={{ fontSize: fontSize.normal, margin: "0px 0px 0px 5px" }}
-              />
-              <FacebookIcon
-                style={{ fontSize: fontSize.normal, margin: "0px 0px 0px 5px" }}
-              />
+              <Text fontSize={fontSize.small}>info@mail.com</Text>
             </CustomDiv>
-          </Grid>
+          </CustomDiv>
+        </Grid>
+        <Grid item md={6} lg={6}>
+          <CustomDiv
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-end"
+            textAlign="right"
+          >
+            <TwitterIcon
+              style={{
+                fontSize: fontSize.normal,
+                color: colors.gray,
+              }}
+            />
+            <InstagramIcon
+              style={{
+                fontSize: fontSize.normal,
+                color: colors.gray,
+              }}
+            />
+            <FacebookIcon
+              style={{
+                fontSize: fontSize.normal,
+                color: colors.gray,
+              }}
+            />
+          </CustomDiv>
         </Grid>
       </Grid>
     </CustomDiv>
