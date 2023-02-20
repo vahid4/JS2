2fc275cdc192d62bb5aab9edef23009fd342ec94
vahid4 <vahid.gasanin1@gmail.com>
2023-02-20 19:51:16 +0100

    styled-components

--- random/src/Templates/Navigation/MainBar.jsx
+++ random/src/Templates/Navigation/MainBar.jsx
@@ -3,11 +3,12 @@ import CustomDiv from "../../components/CustomDiv/CustomDiv";
 import Grid from "@mui/material/Grid";
 import { colors, fontSize } from "../../util/theme";
 import { getScreenWidth } from "../../util/helpers";
+import logo2 from "../../assets/images/logopravi.png";
 
 const MainBar = () => {
   return (
     <CustomDiv
-      bgColor={colors.secondColor}
+      bgColor="white"
       display="flex"
       width="100%"
       height="30px"
@@ -15,7 +16,9 @@ const MainBar = () => {
       border="0px"
     >
       <Grid container direction="row">
-        <Grid item md={6} lg={6}></Grid>
+        <Grid item md={6} lg={6}>
+          <img src={logo2} alt="Logo" width="150" height="auto" />
+        </Grid>
         <Grid item md={6} lg={6}></Grid>
       </Grid>
     </CustomDiv>
