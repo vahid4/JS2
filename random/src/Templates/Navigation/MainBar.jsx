6478c1241248783c9169486f72404d40a647c308
vahid4 <vahid.gasanin1@gmail.com>
2023-02-20 20:05:15 +0100

    styled-components

--- random/src/Templates/Navigation/MainBar.jsx
+++ random/src/Templates/Navigation/MainBar.jsx
@@ -3,7 +3,13 @@ import CustomDiv from "../../components/CustomDiv/CustomDiv";
 import Grid from "@mui/material/Grid";
 import { colors, fontSize } from "../../util/theme";
 import { getScreenWidth } from "../../util/helpers";
-import logo2 from "../../assets/images/logopravi.png";
+import logo from "../../assets/images/logopravi.png";
+
+import PersonIcon from "@mui/icons-material/Person";
+import StarIcon from "@mui/icons-material/Star";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
+import LockIcon from "@mui/icons-material/Lock";
 
 const MainBar = () => {
   return (
@@ -17,7 +23,7 @@ const MainBar = () => {
     >
       <Grid container direction="row">
         <Grid item md={6} lg={6}>
-          <img src={logo2} alt="Logo" width="120" height="auto" />
+          <img src={logo} alt="Logo" width="140" height="auto" />
         </Grid>
         <Grid item md={6} lg={6}></Grid>
       </Grid>
