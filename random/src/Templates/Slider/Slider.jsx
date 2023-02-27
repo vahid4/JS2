e84e8c1bac1c75bbe17c4e92dac6b03f18c54489
vahid4 <vahid.gasanin1@gmail.com>
2023-02-27 20:08:45 +0100

    styled-components

--- random/src/Templates/Slider/Slider.jsx
+++ random/src/Templates/Slider/Slider.jsx
@@ -3,6 +3,14 @@ import Carousel from "../../components/Carousel/Carousel";
 import girl1 from "../../assets/images/home/girl1.jpg";
 import girl2 from "../../assets/images/home/girl2.jpg";
 import girl3 from "../../assets/images/home/girl3.jpg";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+
+const styles = {
+  sliderContainer: {
+    height: "200px",
+    width: "100%",
+  },
+};
 
 const Slider = () => {
   const sliders = [
@@ -20,7 +28,11 @@ const Slider = () => {
     },
   ];
 
-  return <Carousel data={sliders} />;
+  return (
+    <SimplifiedDiv style={styles.sliderContainer}>
+      <Carousel data={sliders} />
+    </SimplifiedDiv>
+  );
 };
 
 export default Slider;
--- random/src/components/Carousel/Carousel.jsx
+++ random/src/components/Carousel/Carousel.jsx
@@ -1,13 +1,27 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel as SliderCarousel } from "react-responsive-carousel";
+import Text from "../Text/Text";
+import { fontSize } from "../../util/theme";
 
 const Carousel = ({ data }) => {
   return (
-    <SliderCarousel>
+    <SliderCarousel
+      width="100%"
+      axis="horizontal"
+      showIndicators
+      showThumbs={false}
+      interval={3000}
+      autoPlay
+    >
       {data.map((d) => (
-        <div>
-          <img src={d.imgSrc} />
-          <p className="legend">{d.imgText}</p>
+        <div key={d} style={{ height: "400px" }}>
+          <img
+            src={d.imgSrc}
+            style={{ height: "100%", objectFit: "contain" }}
+          />
+          <Text className="legend" fontSize={fontSize.xLarge}>
+            {d.imgText}
+          </Text>
         </div>
       ))}
     </SliderCarousel>
