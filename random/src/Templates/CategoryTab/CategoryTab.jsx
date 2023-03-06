486077a9da674bcaffabba74667b70089d4f9e52
vahid4 <vahid.gasanin1@gmail.com>
2023-03-06 20:57:34 +0100

    styled-components

---
+++ random/src/Templates/CategoryTab/CategoryTab.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Text from "../../components/Text/Text";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import { colors } from "../../util/theme";
+
+const CategoryTab = () => {
+  const styles = {
+    container: {
+      borderWidth: "0.5px",
+      borderColor: colors.lightGray,
+      borderRadius: "5px",
+    },
+  };
+
+  return <SimplifiedDiv></SimplifiedDiv>;
+};
+
+export default CategoryTab;
