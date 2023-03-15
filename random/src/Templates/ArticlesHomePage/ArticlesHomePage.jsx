39f13b174e46d3bf26447b1b7883970422a9c563
vahid4 <vahid.gasanin1@gmail.com>
2023-03-15 20:02:28 +0100

    styled-components

--- random/src/Templates/ArticlesHomePage/ArticlesHomePage.jsx
+++ random/src/Templates/ArticlesHomePage/ArticlesHomePage.jsx
@@ -2,9 +2,69 @@ import React from "react";
 import ArticleCard from "../ArticleCard/ArticleCard";
 import { Grid } from "@mui/material";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import article1 from "../../assets/images/home/product1.jpg";
+import article2 from "../../assets/images/home/product2.jpg";
+import article3 from "../../assets/images/home/product3.jpg";
 
 const ArticlesHomePage = () => {
   //Postaviti funkcije koje ce izlistavati artikle
+
+  const articles = [
+    {
+      title: "Article 1",
+      description: "",
+      image: article1,
+      price: "25$",
+      onShowMore: () => {
+        return;
+      },
+    },
+    {
+      title: "Patike",
+      description: "",
+      image: article2,
+      price: "99.99$",
+      onShowMore: () => {
+        return;
+      },
+    },
+    {
+      title: "Jakna",
+      description: "",
+      image: article3,
+      price: "121$",
+      onShowMore: () => {
+        return;
+      },
+    },
+    {
+      title: "Baletanke",
+      description: "",
+      image: article1,
+      price: "17$",
+      onShowMore: () => {
+        return;
+      },
+    },
+    {
+      title: "Kapa",
+      description: "",
+      image: article2,
+      price: "6$",
+      onShowMore: () => {
+        return;
+      },
+    },
+    {
+      title: "Duks",
+      description: "",
+      image: article3,
+      price: "22$",
+      onShowMore: () => {
+        return;
+      },
+    },
+  ];
 
   return (
     <SimplifiedDiv style={{}}>
