de56e3acee960a13b80ee3c2515190f5f3063544
vahid4 <vahid.gasanin1@gmail.com>
2023-03-15 20:29:15 +0100

    styled-components

--- random/src/components/PrimaryButton/PrimaryButton.jsx
+++ random/src/components/PrimaryButton/PrimaryButton.jsx
@@ -17,6 +17,8 @@ const PrimaryButton = styled.button`
   opacity: ${(props) => (props.opacity ? props.opacity : 1)};
   width: ${(props) => (props.width ? props.width : "auto")};
   height: ${(props) => (props.height ? props.height : "auto")};
+  border-radius: ${(props) =>
+    props.borderRadius ? props.borderRadius : "5px"};
 `;
 
 export default PrimaryButton;
