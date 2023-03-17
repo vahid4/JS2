8068b69fdb3aeae7ffd95a8114076023a9069914
vahid4 <vahid.gasanin1@gmail.com>
2023-03-17 20:56:41 +0100

    styled-components

--- random/src/Templates/RecommendedArticles/RecommendedArticles.jsx
+++ random/src/Templates/RecommendedArticles/RecommendedArticles.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-import article1 from "../../assets/images/home/product1.jpg";
-import article2 from "../../assets/images/home/product2.jpg";
-import article3 from "../../assets/images/home/product3.jpg";
-import { Grid } from "@mui/material";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
 import Carousel from "../../components/Carousel/Carousel";
 import ArticleCard from "../ArticleCard/ArticleCard";
