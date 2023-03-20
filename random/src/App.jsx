c4bfc186f7da84b22c0645dd16e52a2c671de507
vahid4 <vahid.gasanin1@gmail.com>
2023-03-20 20:21:31 +0100

    styled-components

--- random/src/App.jsx
+++ random/src/App.jsx
@@ -3,6 +3,23 @@ import Navigation from "./Templates/Navigation/Navigation";
 import ProductHomePage from "./Templates/ProductHomePage/ProductHomePage";
 import Slider from "./Templates/Slider/Slider";
 
+import "./assets/fonts/Lato-Black.ttf";
+import "./assets/fonts/Lato-BlackItalic.ttf";
+import "./assets/fonts/Lato-Bold.ttf";
+import "./assets/fonts/Lato-BoldItalic.ttf";
+import "./assets/fonts/Lato-Italic.ttf";
+import "./assets/fonts/Lato-Light.ttf";
+import "./assets/fonts/Lato-LightItalic.ttf";
+import "./assets/fonts/Lato-Regular.ttf";
+import "./assets/fonts/Lato-Thin.ttf";
+import "./assets/fonts/Lato-ThinItalic.ttf";
+import "./assets/fonts/Montserrat-Black.ttf";
+import "./assets/fonts/Montserrat-BlackItalic.ttf";
+import "./assets/fonts/Montserrat-Bold.ttf";
+import "./assets/fonts/Montserrat-BoldItalic.ttf";
+import "./assets/fonts/Montserrat-ExtraBold.ttf";
+import "./assets/fonts/Montserrat-ExtraLight.ttf";
+
 function App() {
   return (
     <>
