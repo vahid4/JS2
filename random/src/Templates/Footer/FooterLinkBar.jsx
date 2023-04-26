7bbe96fe05e5086f8b275284cd4935a19c467fa9
vahid4 <vahid.gasanin1@gmail.com>
2023-04-26 19:23:37 +0200

    footer

--- random/src/Templates/Footer/FooterLinkBar.jsx
+++ random/src/Templates/Footer/FooterLinkBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
-import { colors, fontWeight } from "../../util/theme";
+import { colors, fontSize, fontWeight } from "../../util/theme";
 import { Grid } from "@mui/material";
 import Text from "../../components/Text/Text";
 
@@ -8,10 +8,10 @@ const FooterLinkBar = () => {
   const styles = {
     mainHolder: {
       backgroundColor: colors.secondColor,
-      padding: "20px",
+      padding: "20px 100px",
     },
     headingText: {
-      textAlign: "center",
+      textAlign: "left",
       fontWeight: fontWeight.mediumBold,
       color: colors.gray,
     },
@@ -19,6 +19,55 @@ const FooterLinkBar = () => {
       textAlign: "left",
       padding: "30px 0px",
     },
+    innerBoxHolder: {
+      textAlign: "left",
+      padding: "20px 0px",
+      lineHeight: "30px",
+    },
+    innerBoxText: {
+      fontSize: fontSize.optima,
+      color: colors.gray,
+    },
+  };
+
+  const FirstRow = () => {
+    <SimplifiedDiv style={styles.innerBoxHolder}>
+      <Text style={styles.innerBoxText}>Online help</Text>
+      <Text style={styles.innerBoxText}>Contact us</Text>
+      <Text style={styles.innerBoxText}>Order status</Text>
+      <Text style={styles.innerBoxText}>Change location</Text>
+      <Text style={styles.innerBoxText}>FAQ</Text>
+    </SimplifiedDiv>;
+  };
+
+  const SecondRow = () => {
+    <SimplifiedDiv style={styles.innerBoxHolder}>
+      <Text style={styles.innerBoxText}>T-shirts</Text>
+      <Text style={styles.innerBoxText}>Mens</Text>
+      <Text style={styles.innerBoxText}>Womens</Text>
+      <Text style={styles.innerBoxText}>Gift cards</Text>
+      <Text style={styles.innerBoxText}>Shoes</Text>
+    </SimplifiedDiv>;
+  };
+
+  const ThirdRow = () => {
+    <SimplifiedDiv style={styles.innerBoxHolder}>
+      <Text style={styles.innerBoxText}>Terms of use</Text>
+      <Text style={styles.innerBoxText}>Privacy policy</Text>
+      <Text style={styles.innerBoxText}>Refund system</Text>
+      <Text style={styles.innerBoxText}>Billing system</Text>
+      <Text style={styles.innerBoxText}>Ticket system</Text>
+    </SimplifiedDiv>;
+  };
+
+  const FourthRow = () => {
+    <SimplifiedDiv style={styles.innerBoxHolder}>
+      <Text style={styles.innerBoxText}>Company information</Text>
+      <Text style={styles.innerBoxText}>Careers</Text>
+      <Text style={styles.innerBoxText}>Store locations</Text>
+      <Text style={styles.innerBoxText}>Affiliate program</Text>
+      <Text style={styles.innerBoxText}>Copyright</Text>
+    </SimplifiedDiv>;
   };
 
   return (
@@ -27,21 +76,25 @@ const FooterLinkBar = () => {
         <Grid item md={2} lg={2}>
           <SimplifiedDiv style={styles.boxHolder}>
             <Text>Service</Text>
+            <FirstRow />
           </SimplifiedDiv>
         </Grid>
         <Grid item md={2} lg={2}>
           <SimplifiedDiv style={styles.boxHolder}>
             <Text>Quick shop</Text>
+            <SecondRow />
           </SimplifiedDiv>
         </Grid>
         <Grid item md={2} lg={2}>
           <SimplifiedDiv style={styles.boxHolder}>
             <Text>Policies</Text>
+            <ThirdRow />
           </SimplifiedDiv>
         </Grid>
         <Grid item md={2} lg={2}>
           <SimplifiedDiv style={styles.boxHolder}>
             <Text>About shopper</Text>
+            <FourthRow />
           </SimplifiedDiv>
         </Grid>
       </Grid>
